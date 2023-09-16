22935ddc3e8367d583e3abac163b01afc7d92e4b
Quin <quin.marilyn05@gmail.com>
2023-09-16 17:45:02 -0600

    Convert the client to pure C.

--- client.c
+++ client.c
@@ -1,10 +1,10 @@
+#include <stdio.h>
+#include <string.h>
 #include <winsock2.h>
-#include <iostream>
-#include <string>
 
 int main(int argc, const char *argv[]) {
 	if (argc != 3) {
-		std::cerr << "Usage: " << argv[0] << " <server> <port>" << std::endl;
+		fprintf(stderr, "Usage: %s <server> <port>\n", argv[0]);
 		return 1;
 	}
 	const char *server = argv[1];
@@ -12,33 +12,34 @@ int main(int argc, const char *argv[]) {
 	const char *message = "kill";
 	WSADATA wsa_data;
 	if (WSAStartup(MAKEWORD(2, 2), &wsa_data) != 0) {
-		std::cerr << "WSAStartup failed" << std::endl;
+		fprintf(stderr, "WSAStartup failed\n");
 		return 1;
 	}
 	SOCKET client_socket = socket(AF_INET, SOCK_STREAM, 0);
 	if (client_socket == INVALID_SOCKET) {
-		std::cerr << "Failed to create socket" << std::endl;
+		fprintf(stderr, "Failed to create socket\n");
 		WSACleanup();
 		return 1;
 	}
-	sockaddr_in server_address{};
+	struct sockaddr_in server_address;
+	memset(&server_address, 0, sizeof(server_address));
 	server_address.sin_family = AF_INET;
-	server_address.sin_port = htons(std::stoi(port));
+	server_address.sin_port = htons(atoi(port));
 	server_address.sin_addr.s_addr = inet_addr(server);
 	if (server_address.sin_addr.s_addr == INADDR_NONE) {
-		std::cerr << "Invalid server address" << std::endl;
+		fprintf(stderr, "Invalid server address\n");
 		closesocket(client_socket);
 		WSACleanup();
 		return 1;
 	}
-	if (connect(client_socket, reinterpret_cast<sockaddr *>(&server_address), sizeof(server_address)) == SOCKET_ERROR) {
-		std::cerr << "Failed to connect to the server" << std::endl;
+	if (connect(client_socket, (struct sockaddr *)&server_address, sizeof(server_address)) == SOCKET_ERROR) {
+		fprintf(stderr, "Failed to connect to the server\n");
 		closesocket(client_socket);
 		WSACleanup();
 		return 1;
 	}
 	if (send(client_socket, message, strlen(message), 0) == SOCKET_ERROR) {
-		std::cerr << "Failed to send data" << std::endl;
+		fprintf(stderr, "Failed to send data\n");
 	}
 	closesocket(client_socket);
 	WSACleanup();
