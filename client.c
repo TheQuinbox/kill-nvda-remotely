c1b3aaa2977859fb26d4c6c0d4f4c360e1351d13
Quin <quin.marilyn05@gmail.com>
2023-09-20 22:57:13 -0600

    Make the client entirely use the INI file, and not a console window.

--- client.c
+++ client.c
@@ -1,48 +1,51 @@
-#include <stdio.h>
-#include <string.h>
 #include <winsock2.h>
 #include <ini.h>
 
 int main(int argc, const char *argv[]) {
-	if (argc != 3) {
-		fprintf(stderr, "Usage: %s <server> <port>\n", argv[0]);
-		return 1;
+	ini_t *config = ini_load("config.ini");
+	if (config == NULL) {
+		MessageBox(NULL, "Configuration file not found", "Error", MB_OK | MB_ICONERROR);
+		ExitProcess(1);
 	}
-	const char *server = argv[1];
-	const char *port = argv[2];
+	const char *host = ini_get(config, NULL, "host");
+	const char *port = ini_get(config, NULL, "port");
+	if (!host || !port) {
+		MessageBox(NULL, "Invalid configuration file", "Error", MB_OK | MB_ICONERROR);
+		ExitProcess(1);
+	}
 	const char *message = "kill";
 	WSADATA wsa_data;
 	if (WSAStartup(MAKEWORD(2, 2), &wsa_data) != 0) {
-		fprintf(stderr, "WSAStartup failed\n");
-		return 1;
+		MessageBox(NULL, "Failed to start the server", "Error", MB_OK | MB_ICONERROR);
+		ExitProcess(1);
 	}
 	SOCKET client_socket = socket(AF_INET, SOCK_STREAM, 0);
 	if (client_socket == INVALID_SOCKET) {
-		fprintf(stderr, "Failed to create socket\n");
+		MessageBox(NULL, "Failed to create socket", "Error", MB_OK | MB_ICONERROR);
 		WSACleanup();
-		return 1;
+		ExitProcess(1);
 	}
 	struct sockaddr_in server_address;
 	memset(&server_address, 0, sizeof(server_address));
 	server_address.sin_family = AF_INET;
 	server_address.sin_port = htons(atoi(port));
-	server_address.sin_addr.s_addr = inet_addr(server);
+	server_address.sin_addr.s_addr = inet_addr(host);
 	if (server_address.sin_addr.s_addr == INADDR_NONE) {
-		fprintf(stderr, "Invalid server address\n");
+		MessageBox(NULL, "Invalid server address", "Error", MB_OK | MB_ICONERROR);
 		closesocket(client_socket);
 		WSACleanup();
-		return 1;
+		ExitProcess(1);
 	}
 	if (connect(client_socket, (struct sockaddr *)&server_address, sizeof(server_address)) == SOCKET_ERROR) {
-		fprintf(stderr, "Failed to connect to the server\n");
+		MessageBox(NULL, "Failed to connect to the server", "Error", MB_OK | MB_ICONERROR);
 		closesocket(client_socket);
 		WSACleanup();
-		return 1;
+		ExitProcess(1);
 	}
 	if (send(client_socket, message, strlen(message), 0) == SOCKET_ERROR) {
-		fprintf(stderr, "Failed to send data\n");
+		MessageBox(NULL, "Failed to send data", "Error", MB_OK | MB_ICONERROR);
 	}
 	closesocket(client_socket);
 	WSACleanup();
-	return 0;
+	ExitProcess(0);;
 }
