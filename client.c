054b1cc538937c502d5c90d5c66e8b040dc7c594
Quin <TheQuinbox@gmail.com>
2025-02-20 10:36:32 -0700

    It works!

--- client.c
+++ client.c
@@ -1,16 +1,25 @@
 #define WIN32_LEAN_AND_MEAN
 #include <windows.h>
 #include <winsock2.h>
+#include <shlwapi.h>
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR szCmdLine, int iCmdShow) {
-	char* pszHost = {0};
-	char* pszPort = {0};
-	const char* pszConfigFile = "config.ini";
-	if (GetPrivateProfileString("Settings", "Host", NULL, pszHost, sizeof(pszHost), pszConfigFile) == 0 || GetPrivateProfileString("Settings", "Port", NULL, pszPort, sizeof(pszPort), pszConfigFile) == 0) {
+	char szHost[256] = {0};;
+	char szPort[16] = {0};
+	char szConfigFile[MAX_PATH] = {0};
+	if (GetCurrentDirectory(sizeof(szConfigFile), szConfigFile) == 0) {
+		MessageBox(NULL, "Failed to get the current working directory", "Error", MB_OK | MB_ICONERROR);
+		ExitProcess(1);
+	}
+	if (!PathCombine(szConfigFile, szConfigFile, "config.ini")) {
+		MessageBox(NULL, "Failed to construct the config file path", "Error", MB_OK | MB_ICONERROR);
+		ExitProcess(1);
+	}
+	if (GetPrivateProfileString("Settings", "Host", NULL, szHost, sizeof(szHost), szConfigFile) == 0 || GetPrivateProfileString("Settings", "Port", NULL, szPort, sizeof(szPort), szConfigFile) == 0) {
 		MessageBox(NULL, "Invalid or missing configuration file", "Error", MB_OK | MB_ICONERROR);
 		ExitProcess(1);
 	}
-	const char* pszMessage = "kill";
+	const char* szMessage = "kill";
 	WSADATA wsaData;
 	if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
 		MessageBox(NULL, "Failed to initialize Winsock", "Error", MB_OK | MB_ICONERROR);
@@ -24,8 +33,8 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR szCmdLine,
 	}
 	struct sockaddr_in stServerAddress = {0};
 	stServerAddress.sin_family = AF_INET;
-	stServerAddress.sin_port = htons((u_short)atoi(pszPort));
-	stServerAddress.sin_addr.s_addr = inet_addr(pszHost);
+	stServerAddress.sin_port = htons((u_short)atoi(szPort));
+	stServerAddress.sin_addr.s_addr = inet_addr(szHost);
 	if (stServerAddress.sin_addr.s_addr == INADDR_NONE) {
 		MessageBox(NULL, "Invalid server address", "Error", MB_OK | MB_ICONERROR);
 		closesocket(hClientSocket);
@@ -38,7 +47,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR szCmdLine,
 		WSACleanup();
 		ExitProcess(1);
 	}
-	if (send(hClientSocket, pszMessage, strlen(pszMessage), 0) == SOCKET_ERROR) {
+	if (send(hClientSocket, szMessage, strlen(szMessage), 0) == SOCKET_ERROR) {
 		MessageBox(NULL, "Failed to send data", "Error", MB_OK | MB_ICONERROR);
 	}
 	closesocket(hClientSocket);
